d97bb6f3c8c1ac4b22f53578425c6b1aba5e6de3
BrackebuschK <kristopher@goodcode.net>
2024-07-18 06:27:02 -0700

    Add files via upload

--- updatePi.sh
+++ updatePi.sh
@@ -4,10 +4,11 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt full-upgrade -y
-sudo apt install rpi-update
+#sudo apt install rpi-update
 
 # Perform firmware update
-sudo rpi-update
+#sudo rpi-update -- took this off because it's a bad idea
+#sudo rpi-eeprom-update -- thought about using this but decided not to
 
 # Clean up unnecessary packages
 sudo apt autoremove -y
